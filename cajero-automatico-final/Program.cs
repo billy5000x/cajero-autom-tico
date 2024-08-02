b66b98e21a29cd35dffe853b67bd48528a61417a
Billy5000x <jhoansnf02@gmail.com>
2024-08-02 14:15:44 -0500

    Se crea la rama Buscar cuenta con codigo

--- cajero-automatico-final/Program.cs
+++ cajero-automatico-final/Program.cs
@@ -1,40 +1,11 @@
-﻿static void OperarCajero(int cuentaRegistro)
+﻿static int BuscarCuenta(string cuenta, string contraseña)
 {
-    int opcion;
-
-    do
+    for (int i = 0; i < cuentas.GetLength(0); i++)
     {
-        System.Threading.Thread.Sleep(4000);
-        Console.Clear();
-        MostrarMenu();
-        opcion = LeerOpcion();
-
-        switch (opcion)
+        if (cuentas[i, 0] == cuenta && VerificarContraseña(contraseña, cuentas[i, 1]))
         {
-            case 1:
-                RealizarRetiro(cuentaRegistro);
-                break;
-            case 2:
-                MostrarMovimientos(cuentaRegistro);
-                break;
-            case 3:
-                RealizarDeposito(cuentaRegistro);
-                break;
-            case 4:
-                CambiarClave(cuentaRegistro);
-                break;
-            case 5:
-                ConsultarSaldo(cuentaRegistro);
-                break;
-            case 6:
-                RealizarTransferencia(cuentaRegistro);
-                break;
-            case 7:
-                Console.WriteLine("Gracias por utlizar nuestros servios.");
-                return;
-            default:
-                Console.WriteLine("Opción invalida por favor, seleccione una Opción valida.");
-                break;
+            return i;
         }
-    } while (true);
+    }
+    return -1;
 }
