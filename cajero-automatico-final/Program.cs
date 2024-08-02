a85e139bcbe3c0b0204af5c60a35f12369120a58
Billy5000x <jhoansnf02@gmail.com>
2024-08-02 13:59:34 -0500

    crear rama y codigo

--- cajero-automatico-final/Program.cs
+++ cajero-automatico-final/Program.cs
@@ -106,7 +106,7 @@ class CajeroAutomatico
         Console.WriteLine("1. Retiro");
         Console.WriteLine("2. Movimientos");
         Console.WriteLine("3. Deposito");
-        Console.WriteLine("4. Cambio de clave");
+        Console.WriteLine("4. Cambio de claves");
         Console.WriteLine("5. Consulta de saldo");
         Console.WriteLine("6. Transferencia");
         Console.WriteLine("7. salir ");
