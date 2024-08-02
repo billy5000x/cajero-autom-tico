e6d2df30892c53a90350fc8beae66d827606cb11
Billy5000x <jhoansnf02@gmail.com>
2024-08-02 14:26:33 -0500

    se crea la rama funcion Realizar deposito

--- cajero-automatico-final/Program.cs
+++ cajero-automatico-final/Program.cs
@@ -1,4 +1,19 @@
-﻿static string[] ObtenerMovimientos(string cuenta)
+﻿static void RealizarDeposito(int cuentaRegistro)
 {
-    return File.ReadAllLines($"{cuenta}_movimientos.txt");
+    Console.WriteLine("ingrese el monto a deposotar");
+    double monto;
+    while (!double.TryParse(Console.ReadLine(), out monto) || monto <= 0)
+    {
+        Console.WriteLine("monto invalido por favor ingrese un monto valido");
+    }
+
+    double saldo = double.Parse(cuentas[cuentaRegistro, 2]);
+
+    saldo += monto;
+    cuentas[cuentaRegistro, 2] = saldo.ToString();
+    Console.WriteLine($"Se ha depositado ${monto}. Nuevo saldo: ${saldo}");
+
+    string cuenta = cuentas[cuentaRegistro, 0];
+    string[] movimiento = { $"Deposito: ${monto}. Nuevo saldo: ${saldo}" };
+    File.AppendAllLines($"{cuenta}_movimientos.txt", movimiento);
 }
