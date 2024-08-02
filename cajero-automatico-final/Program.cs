e9d9aad46ccf5d21ead3b3d80ae6090c4d1de405
Billy5000x <jhoansnf02@gmail.com>
2024-08-02 14:11:25 -0500

    se crea la rama funcion main con codigo

--- cajero-automatico-final/Program.cs
+++ cajero-automatico-final/Program.cs
@@ -1,24 +1,39 @@
-﻿static void RealizarRetiro(int cuentaRegistro)
+﻿static void Main(string[] args)
 {
-    Console.WriteLine("Ingrese el monto a retirar.");
-    double monto;
-    while (!double.TryParse(Console.ReadLine(), out monto) || monto < 0)
+    // apertura de cuentas
+    cuentas[0, 0] = "253090"; //Numero de cuenta
+    cuentas[0, 1] = ("1346"); //clave
+    cuentas[0, 2] = "1800000.00"; //saldo inicial 
+
+    cuentas[1, 0] = "569543"; //Numero de cuentas 
+    cuentas[1, 1] = ("1245"); //clave
+    cuentas[1, 2] = "1250000.00"; //saldo
+
+    cuentas[2, 0] = "794613"; //Numero de cuenta
+    cuentas[2, 1] = ("8956"); //clave
+    cuentas[2, 2] = "2000000.00"; //saldo
+
+    string cuenta, contraseña;
+    int cuentaRegistro;
+
+    do
     {
-        Console.WriteLine("Monto invalido. Por favor, ingrese un monto valido.");
-    }
+        Console.WriteLine("Bienvenido al Cajero Automático");
+        Console.WriteLine("Por favor ingrese su numero de cuenta");
+        cuenta = Console.ReadLine();
 
-    double saldo = double.Parse(cuentas[cuentaRegistro, 2]);
+        Console.WriteLine("Ingrese su Contraseña");
+        contraseña = Console.ReadLine();
 
-    if (monto > saldo)
-    {
-        Console.WriteLine("Saldo insuficiente.");
-        return;
-    }
+        cuentaRegistro = BuscarCuenta(cuenta, contraseña);
 
-    saldo -= monto;
-    cuentas[cuentaRegistro, 2] = saldo.ToString();
-    Console.WriteLine($"Se a retirado ${monto}. Nuevo saldo: ${saldo}");
-    string cuenta = cuentas[cuentaRegistro, 0];
-    string[] reti = { $"Se ha retirado ${monto}. Nuevo saldo: ${saldo}" };
-    File.AppendAllLines($"{cuenta}_Factura-electronica.txt", reti);
+        if (cuentaRegistro != -1)
+        {
+            OperarCajero(cuentaRegistro);
+        }
+        else
+        {
+            Console.WriteLine("Numero de Cuenta o Contraseña incorrecta.  por favor, intentelo de nuevo.");
+        }
+    } while (true);
 }
