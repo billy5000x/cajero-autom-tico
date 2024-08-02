610929fb01ef53fe64fb0a350adb9413005d9a8f
Billy5000x <jhoansnf02@gmail.com>
2024-08-02 14:30:08 -0500

    se crea la funcion Cambiar clave

--- cajero-automatico-final/Program.cs
+++ cajero-automatico-final/Program.cs
@@ -1,19 +1,7 @@
-﻿static void RealizarDeposito(int cuentaRegistro)
+﻿static void CambiarClave(int cuentaRegistro)
 {
-    Console.WriteLine("ingrese el monto a deposotar");
-    double monto;
-    while (!double.TryParse(Console.ReadLine(), out monto) || monto <= 0)
-    {
-        Console.WriteLine("monto invalido por favor ingrese un monto valido");
-    }
-
-    double saldo = double.Parse(cuentas[cuentaRegistro, 2]);
-
-    saldo += monto;
-    cuentas[cuentaRegistro, 2] = saldo.ToString();
-    Console.WriteLine($"Se ha depositado ${monto}. Nuevo saldo: ${saldo}");
-
-    string cuenta = cuentas[cuentaRegistro, 0];
-    string[] movimiento = { $"Deposito: ${monto}. Nuevo saldo: ${saldo}" };
-    File.AppendAllLines($"{cuenta}_movimientos.txt", movimiento);
+    Console.WriteLine("Ingrese su nueva clave:");
+    string nuevaClave = Console.ReadLine();
+    cuentas[cuentaRegistro, 1] = (nuevaClave); // actualizar la contraseña 
+    Console.WriteLine("Clave cambiada correctamente.");
 }
