0644f5bc15fee251b5d5e8f079ee8626256a0114
Billy5000x <jhoansnf02@gmail.com>
2024-08-02 14:14:11 -0500

    se crea la rama Funcion operacionCajero Con codigo

--- cajero-automatico-final/Program.cs
+++ cajero-automatico-final/Program.cs
@@ -1,39 +1,40 @@
-﻿static void Main(string[] args)
+﻿static void OperarCajero(int cuentaRegistro)
 {
-    // apertura de cuentas
-    cuentas[0, 0] = "253090"; //Numero de cuenta
-    cuentas[0, 1] = ("1346"); //clave
-    cuentas[0, 2] = "1800000.00"; //saldo inicial 
-
-    cuentas[1, 0] = "569543"; //Numero de cuentas 
-    cuentas[1, 1] = ("1245"); //clave
-    cuentas[1, 2] = "1250000.00"; //saldo
-
-    cuentas[2, 0] = "794613"; //Numero de cuenta
-    cuentas[2, 1] = ("8956"); //clave
-    cuentas[2, 2] = "2000000.00"; //saldo
-
-    string cuenta, contraseña;
-    int cuentaRegistro;
+    int opcion;
 
     do
     {
-        Console.WriteLine("Bienvenido al Cajero Automático");
-        Console.WriteLine("Por favor ingrese su numero de cuenta");
-        cuenta = Console.ReadLine();
+        System.Threading.Thread.Sleep(4000);
+        Console.Clear();
+        MostrarMenu();
+        opcion = LeerOpcion();
 
-        Console.WriteLine("Ingrese su Contraseña");
-        contraseña = Console.ReadLine();
-
-        cuentaRegistro = BuscarCuenta(cuenta, contraseña);
-
-        if (cuentaRegistro != -1)
+        switch (opcion)
         {
-            OperarCajero(cuentaRegistro);
-        }
-        else
-        {
-            Console.WriteLine("Numero de Cuenta o Contraseña incorrecta.  por favor, intentelo de nuevo.");
+            case 1:
+                RealizarRetiro(cuentaRegistro);
+                break;
+            case 2:
+                MostrarMovimientos(cuentaRegistro);
+                break;
+            case 3:
+                RealizarDeposito(cuentaRegistro);
+                break;
+            case 4:
+                CambiarClave(cuentaRegistro);
+                break;
+            case 5:
+                ConsultarSaldo(cuentaRegistro);
+                break;
+            case 6:
+                RealizarTransferencia(cuentaRegistro);
+                break;
+            case 7:
+                Console.WriteLine("Gracias por utlizar nuestros servios.");
+                return;
+            default:
+                Console.WriteLine("Opción invalida por favor, seleccione una Opción valida.");
+                break;
         }
     } while (true);
 }
