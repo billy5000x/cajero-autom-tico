94b77aeda87b212648e42e68958c52f84a357e77
Billy5000x <jhoansnf02@gmail.com>
2024-08-02 14:18:25 -0500

    se crea la rama leeropciones con codigo

--- cajero-automatico-final/Program.cs
+++ cajero-automatico-final/Program.cs
@@ -1,12 +1,9 @@
-﻿static void MostrarMenu()
+﻿static int LeerOpcion()
 {
-    Console.WriteLine("\nMenú de opciones:");
-    Console.WriteLine("1. Retiro");
-    Console.WriteLine("2. Movimientos");
-    Console.WriteLine("3. Deposito");
-    Console.WriteLine("4. Cambio de claves");
-    Console.WriteLine("5. Consulta de saldo");
-    Console.WriteLine("6. Transferencia");
-    Console.WriteLine("7. salir ");
-    Console.WriteLine("Seleccione una opción");
+    int opcion;
+    while (!int.TryParse(Console.ReadLine(), out opcion))
+    {
+        Console.WriteLine("Opción invalida. por favor, seleccione una opción valida.");
+    }
+    return opcion;
 }
